10d3c9a99553cd6db9c8ae84eca5654f246315b0
jhuerta6 <jorgehuertahu@gmail.com>
2017-10-05 19:58:28 -0600

    Diseñando desde nuevo bosquejo

--- index.php
+++ index.php
@@ -88,18 +88,17 @@
       <!-- end videos-->
 
       <div class="row">
-        <div class="col s8">
+        <div class="col s8 center-align">
           <div class="row">
-            <p> lo nuevo </p>
+            <p> [lo nuevo, beta] </p>
           </div>
           <div class="row">
-            <p> haz el cambio </p>
+            <p> [haz el cambio, parallax image] </p>
           </div>
         </div>
-        <div class="col s3">
-          <p> facebook </p>
+        <div class="col s3 center-align">
+          <p> [facebook embed] </p>
         </div>
-
     </div>
     <!--<div class="container"> -->
   </body>
@@ -113,6 +112,29 @@
   </script>
 
 </main>
-<footer>
-</footer>
+  <footer class="page-footer black">
+     <div class="container">
+       <div class="row">
+         <div class="col l6 s12">
+           <h5 class="white-text">Footer Content Beta</h5>
+           <p class="grey-text text-lighten-4">Lorem ipsum blah blah blah.</p>
+         </div>
+         <div class="col l4 offset-l2 s12">
+           <h5 class="white-text">Links</h5>
+           <ul>
+             <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+             <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+             <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+             <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+           </ul>
+         </div>
+       </div>
+     </div>
+     <div class="footer-copyright">
+       <div class="container">
+       © 2017 HN Solutions
+       <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+       </div>
+     </div>
+   </footer>
 </html>
